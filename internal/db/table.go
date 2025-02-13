510736ad64164fefa1a87adcbd55fdfcad8b7864
zipkero <zipkero@gmail.com>
2025-02-13 23:18:43 +0900

    컬럼 데이터 타입 변경

--- internal/db/table.go
+++ internal/db/table.go
@@ -3,9 +3,10 @@ package db
 import "fmt"
 
 type Table struct {
-	Name    string
-	Columns []*Column
-	Storage *Storage
+	Name        string
+	Columns     map[string]*Column
+	ColumnOrder []string
+	Storage     *Storage
 }
 
 func newTable(name string, columns []*Column) (*Table, error) {
@@ -13,13 +14,28 @@ func newTable(name string, columns []*Column) (*Table, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error creating new table: %v", err)
 	}
+
+	colMap := make(map[string]*Column)
+	colOrder := make([]string, len(columns))
+
+	for i, col := range columns {
+		colMap[col.Name] = col
+		colOrder[i] = col.Name
+	}
+
 	return &Table{
-		Name:    name,
-		Columns: columns,
-		Storage: storage,
+		Name:        name,
+		Columns:     colMap,
+		ColumnOrder: colOrder,
+		Storage:     storage,
 	}, nil
 }
 
-func (t *Table) AddColumn(column *Column) {
-	t.Columns = append(t.Columns, column)
+func (t *Table) AddColumn(column *Column) error {
+	if _, ok := t.Columns[column.Name]; ok {
+		return fmt.Errorf("column %s already exists", column.Name)
+	}
+	t.Columns[column.Name] = column
+	t.ColumnOrder = append(t.ColumnOrder, column.Name)
+	return nil
 }
